848a2dd3946d1d81d27c74ab9bf8a536e452b301
Mukesh N <96813673+Mukesh-Uchiha@users.noreply.github.com>
2023-03-08 09:26:52 +0530

    Update resume.js

--- script/resume.js
+++ script/resume.js
@@ -2,14 +2,12 @@ $(document).ready(function () {
   $(window).load(function () {
     $("#button").click(function (e) {
       $("#button").css("background", "#00b50f");
-      $("#button").css("color", "#1d1d1d");
       $("#button").css("border-color", "#1d1d1d");
       $("#button").css("letter-spacing", "0");
       $("#button").html('Downloaded  <i class="fa-solid fa-check"></i>');
     });
     function move() {
       $("#button").css("background", "#1d1d1d");
-      $("#button").css("color", "#00adb5");
       $("#button").css("border-color", "#00adb5");
       $("#button").css("letter-spacing", "0");
       $("#button").html(' Download Resume <i class="fa-solid fa-download">');
